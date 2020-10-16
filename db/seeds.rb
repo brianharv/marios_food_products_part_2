bc4482519c3a408cc3fcf390cb69e83cb0bec5eb
Brian Harvey <brian.harv3y@gmail.com>
2020-10-16 15:08:22 -0700

    add review seeds but rating is not working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Food.ingredient,
+ @product = Product.create!(name: Faker::Food.ingredient,
                         cost: Faker::Number.decimal(l_digits: 2),
-                        country_of_origin: Faker::Lorem.word)
-end
+                        country_of_origin: Faker::Address.country)
+    @product.save                    
+      5.times do|index|
+      @review = Review.create!(author: Faker::Name.name,
+                              content_body: Faker::Lorem.paragraph(sentence_count: 5),
+                              rating: rand(1..5),
+                              product_id: @product.id)
+          @review.save
+      end 
+  end                          
 
 p "Created #{Product.count} Products"
+p "Created #{Review.count} Reviews!"
