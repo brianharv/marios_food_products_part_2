55a162cdb3cd5496daf083b4a9a9b9f6bd957bbc
Brian Harvey <brian.harv3y@gmail.com>
2020-10-16 14:04:04 -0700

    add titleize callback

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ApplicationRecord
   has_many :reviews
   validates :name, :cost, :country_of_origin, presence: true
+  before_save(:titleize_name)
+
+  private
+  def titleize(user_input)
+    input_array = []
+    user_input = user_input.split(' ')
+    user_input.each do |word|
+      input_array.push((word.capitalize))
+    end
+    input_array.join(' ')  
+  end  
+
+  def titleize_name
+    self.name = self.name.titleize
+  end  
 end  
