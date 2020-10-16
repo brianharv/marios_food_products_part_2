9b0350600b015164f68420502f2d4e377d99aafa
Brian Harvey <brian.harv3y@gmail.com>
2020-10-16 16:32:53 -0700

    add integration tests for create and delete

---
+++ spec/features/add_new_review_spec.rb
@@ -0,0 +1,24 @@
+describe "the process to add a new review" do
+  it "adds a new review" do
+    product = Product.new({:name => "Blue Cheese", :cost => 2.54, :country_of_origin => "France"})
+    product.save
+    visit products_path
+    click_on 'Blue Cheese'
+    click_on 'Add a review'
+    fill_in 'Author', :with => 'Brian Harvey'
+    fill_in 'Content body', :with => 'This is some text which needs to be at least fifty characters in length. Yadda yadda yadda yadda yadda yadda.'
+    select('3', from: 'Rating')
+    click_on 'Create Review'
+    expect(page).to have_content("New Review Created Successfully")
+    expect(page).to have_content("Brian Harvey")
+  end
+
+  it "gives an error when no fields are entered" do
+    product = Product.new({:name => "Blue Cheese", :cost => 2.54, :country_of_origin => "France"})
+    product.save
+    visit products_path
+    click_on 'Blue Cheese'
+    click_on 'Add a review'
+    expect(page).to have_content("There was an error while creating Review")
+  end
+end
---
+++ spec/features/delete_product_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "the process to delete a product" do
+  it "deletes a product" do
+    product = Product.new({:name => "Blue Cheese", :cost => 2.54, :country_of_origin => "France"})
+    product.save
+    visit products_path
+    click_on 'Blue Cheese'
+    click_on 'Delete product'
+    expect(page).to have_content("This Product has been deleted")
+    expect(page).to_not have_content("Blue Cheese")
+  end
+end  
