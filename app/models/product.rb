cde2658abc3fa9b123289188075e292c7aa2dedb
Brian Harvey <brian.harv3y@gmail.com>
2020-10-16 15:40:29 -0700

    solved issue where the dependent: :destory was on wrong class.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, :cost, :country_of_origin, presence: true
   before_save(:titleize_name)
 
